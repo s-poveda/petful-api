e3cec21b9ff68d82f774e13b9180b4b6c03110fa
sam p <spoveda545@gmail.com>
2020-12-11 15:14:41 -0500

    reformatting for consistency

--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -1,54 +1,54 @@
 class Queue {
-	generateQitem(data, next = null) {
-		return {
-			value: data,
-			last: null,
-			next,
-		}
-	}
+  generateQitem(data, next = null) {
+    return {
+      value: data,
+      last: null,
+      next,
+    };
+  }
 
   constructor() {
     // Set initial data.
     this.head = null;
-		this.tail = null;
-		this.length = 0;
+    this.tail = null;
+    this.length = 0;
   }
 
   enqueue(data) {
     // Add some data to the queue.
-		//if there is nothing in the q
+    //if there is nothing in the q
     if (this.head === null) {
-			this.head = this.generateQitem(data);
-		}
-		//if there is only the head
-		else if (this.tail === null) {
-			this.tail = this.head;
-			this.head = this.generateQitem(data, this.tail);
-			this.tail.last = this.head;
-		}
+      this.head = this.generateQitem(data);
+    }
+    //if there is only the head
+    else if (this.tail === null) {
+      this.tail = this.head;
+      this.head = this.generateQitem(data, this.tail);
+      this.tail.last = this.head;
+    }
 		else {
-			const oldHead = this.head;
-			this.head = this.generateQitem(data, oldHead);
-			oldHead.last = this.head;
-		}
-		this.length++;
+      const oldHead = this.head;
+      this.head = this.generateQitem(data, oldHead);
+      oldHead.last = this.head;
+    }
+    this.length++;
   }
 
   dequeue() {
-		//remove the oldest task from the queue
-		if (this.length === 1) {
-			const data = this.head;
-			this.length--;
-			this.head = null;
-			return data;
-		}
-		const oldTail = this.tail;
-		this.tail = this.tail.last;
-		this.tail.next = null;
-		oldTail.last = null;
-		oldTail.next = null;
-		this.length--;
-		return oldTail;
+    //remove the oldest task from the queue
+    if (this.length === 1) {
+      const data = this.head;
+      this.length--;
+      this.head = null;
+      return data;
+    }
+    const oldTail = this.tail;
+    this.tail = this.tail.last;
+    this.tail.next = null;
+    oldTail.last = null;
+    oldTail.next = null;
+    this.length--;
+    return oldTail;
   }
 
   show() {
@@ -59,13 +59,13 @@ class Queue {
   all() {
     // Return all items in the queue.
     const queue = new Array(this.length);
-		let item = this.tail;
-		for (let i = 0; i < this.length; i++) {
-			queue[i] = item;
-			item = item.last;
-		}
-		return queue;
+    let item = this.tail;
+    for (let i = 0; i < this.length; i++) {
+      queue[i] = item;
+      item = item.last;
+    }
+    return queue;
   }
 }
 
-module.exports = Queue
+module.exports = Queue;
