f248c021bb4118448b38104e81d549c5bc47c82b
sam p <spoveda545@gmail.com>
2020-12-11 15:29:42 -0500

    generic error handler

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
   PORT: process.env.PORT,
+	NODE_ENV: process.env.NODE_ENV || 'production',
 };
--- modules/app/app.js
+++ modules/app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { NODE_ENV } = require('../../config');
 
 const app = express();
 
@@ -7,5 +8,17 @@ app.use(cors());
 
 app.use('/people', require('../people/people.router'));
 app.use('/pets', require('../pets/pets.router'));
+
+app.use((error, req, res, next) => {
+	let message = '';
+	if (NODE_ENV === development) {
+		console.log(error);
+		message = error.message;
+	}
+	else {
+		message = 'Server Error';
+	}
+	res.json({message});
+});
 
 module.exports = app;
